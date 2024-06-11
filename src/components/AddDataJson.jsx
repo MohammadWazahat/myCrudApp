efe7679053d86fa61116965c55e2339abf5bf348
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-06-11 19:28:05 +0530

    form to add api in json data created

--- src/components/AddDataJson.jsx
+++ src/components/AddDataJson.jsx
@@ -1,66 +1,82 @@
-// import React, { useState } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-// const AddDataJson = () => {
-//    const [name,setName] = useState()
-//    const [firstName,setFirstName] = useState()
-//    const [lastName,setLastName] = useState()
-
-
-//   const inputHandlerFistName = (e) => {
-//     e.target.value ;
-//     setFirstName(e.target.value)
-//   };
-
-//   const inputHandlerLastName = (e) => {
-//     e.target.value ;
-//     setLastName(e.target.value)
-//   };
-
-//   const submitForm = (e) => {
-//     e.preventDefault();
-//     setName({ ...name, firstName });
-//     console.log(name)
-//   };
-
-
-//   return (
-//     <div>
-//       <h2>i will add data form</h2>
-//       {firstName}{lastName}
-//       <form onSubmit={submitForm}>
-//         <label htmlFor="">Enter First Name</label>
-//         <input
-//           type="text"
-//           placeholder="Fisrt name"
-//           name="firstName "
-//           onChange={inputHandlerFistName}
-//         />
-//         <label htmlFor="">Enter Last Name</label>
-//         <input
-//           type="text"
-//           placeholder="Last name"
-//           name="lastName"
-//           onChange={inputHandlerLastName}
-//         />
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default AddDataJson;
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 
 const AddDataJson = () => {
+  const [values, setValues] = useState({
+    first_name: "",
+    last_name: "",
+    gender: "",
+    city: "",
+  });
+
+  const navigate = useNavigate();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3030/users", values)
+      .then((res) => {
+        console.log(res);
+        // hook to navigate back to the page
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-           <div>
-        <Link to="/" >user list</Link>
+      <div>
+        <div>
+          <Link to="/">user list</Link>
         </div>
-      add data
-    </div>
-  )
-}
+        add data
+      </div>
 
-export default AddDataJson
+      <h2>i will add data form</h2>
+
+      <form onSubmit={submitForm}>
+        <label htmlFor="first_name">Enter First Name</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Fisrt name"
+          name="first_name"
+          onChange={(e) => setValues({ ...values, first_name: e.target.value })}
+        />
+        <br />
+        <label htmlFor="last_name">Enter Last Name</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Last name"
+          name="last_name"
+          onChange={(e) => setValues({ ...values, last_name: e.target.value })}
+        />
+        <br />
+        <label htmlFor="gender">Enter gender</label>
+        <br />
+        <input
+          type="text"
+          placeholder="gender"
+          name="gender"
+          onChange={(e) => setValues({ ...values, gender: e.target.value })}
+        />
+        <br />
+        <label htmlFor="city">Enter city</label>
+        <br />
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={(e) => setValues({ ...values, city: e.target.value })}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddDataJson;
